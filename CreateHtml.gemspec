fd997ce12c113ee06afec03a8746c2de8ef6e9b4
Sergey <posergey1995@gmail.com>
2021-12-06 00:34:43 +0200

    ...

--- CreateHtml.gemspec
+++ CreateHtml.gemspec
@@ -1,12 +1,12 @@
 Gem::Specification.new do |s|
   s.name        = 'CreateHtml'
   s.version     = '0.1.0'
-  s.summary     = "CreateHtml"
-  s.description = "Making html for Tamagochi"
-  s.authors     = ["Sergey Ponomarenko"]
+  s.summary     = 'CreateHtml'
+  s.description = 'Making html for Tamagochi'
+  s.authors     = ['Sergey Ponomarenko']
   s.email       = 'posergey1995@gmail.com'
-  s.files       = ["lib/CreateHtml.rb"]
+  s.files       = ['lib/CreateHtml.rb']
   s.homepage    =
     ''
-  s.license       = 'MIT'
+  s.license = 'MIT'
 end
--- lib/CreateHtml.rb
+++ lib/CreateHtml.rb
@@ -1,24 +1,20 @@
 module CreateHtml
-
   def html
-
     content = File.read('index.html')
 
     File.open('pet_html.html', 'w') do |file|
-      content.gsub!('{{animal}}', self.animal)
-      content.gsub!('{{name}}', self.name)
-      content.gsub!('{{health}}', "#{self.health}")
-      content.gsub!('{{bellyful}}', "#{self.bellyful}")
-      content.gsub!('{{peppiness}}', "#{self.peppiness}")
-      content.gsub!('{{mood}}', "#{self.mood}")
-      content.gsub!('{{purity}}', "#{self.purity}")
-      content.gsub!('{{toilet}}', "#{self.toilet}")
-      content.gsub!('{{response}}', "#{self.response.uniq.join('__')}")
+      content.gsub!('{{animal}}', animal)
+      content.gsub!('{{name}}', name)
+      content.gsub!('{{health}}', health.to_s)
+      content.gsub!('{{bellyful}}', bellyful.to_s)
+      content.gsub!('{{peppiness}}', peppiness.to_s)
+      content.gsub!('{{mood}}', mood.to_s)
+      content.gsub!('{{purity}}', purity.to_s)
+      content.gsub!('{{toilet}}', toilet.to_s)
+      content.gsub!('{{response}}', response.uniq.join('__').to_s)
       content.gsub!('__', '<br>')
-      content.gsub!('{{emoji}}', "‍#{self.emoji}")
-      file.write("#{content}")
-
-    end
-
+      content.gsub!('{{emoji}}', "‍#{emoji}")
+      file.write(content.to_s)
     end
   end
+end
